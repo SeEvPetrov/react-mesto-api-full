14343cc34dc53e918f5cddf31a3edfd6924e4edc
Sergei Petrov <seevpetrov@gmail.com>
2022-09-27 09:07:15 +0300

    Отредактирвано

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const { PORT = 3000, JWT_SECRET = 'a0d09c51-712e-4f6f-b569-ab027c2c1fac' } = process.env;
 const SALT_LENGTH = 10;
 const DATABASE_URL = 'mongodb://localhost:27017/mestodb';
